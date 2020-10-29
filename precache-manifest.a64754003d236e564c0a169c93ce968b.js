1943fd67fa6a89231d0b1b4373c4f0e331f362c1
褚煦露 <chuxulu@lezhixing.com.cn>
2020-10-29 19:18:13 +0800

    deploy

---
+++ precache-manifest.a64754003d236e564c0a169c93ce968b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0130e2fa3fa40b2ee821",
+    "url": "/monrey-3-website/css/app.72bf3a92.css"
+  },
+  {
+    "revision": "97dc7d442e12bf24c3673f49fffdb7d8",
+    "url": "/monrey-3-website/index.html"
+  },
+  {
+    "revision": "0130e2fa3fa40b2ee821",
+    "url": "/monrey-3-website/js/app.8b13dfbf.js"
+  },
+  {
+    "revision": "e1fb67e49f8f6d230a03",
+    "url": "/monrey-3-website/js/chunk-vendors.2b98eaa0.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/monrey-3-website/manifest.json"
+  },
+  {
+    "revision": "c6b6553c76c082ca655beddaf21799b0",
+    "url": "/monrey-3-website/qrqd.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/monrey-3-website/robots.txt"
+  }
+]);
